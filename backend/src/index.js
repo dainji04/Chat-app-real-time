1da96bdb77f5ae606708020cd4b7bf847be52b7d
dainji04 <ndai31072004@gmail.com>
2025-07-28 09:35:11 +0700

    initial project: backend and front end

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const env = require('dotenv');
+
+env.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(
+        `Server is running on port http://localhost:${process.env.PORT || 3000}`
+    );
+});

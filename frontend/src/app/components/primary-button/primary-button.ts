77395b065dab2894a365941171fd4a5b6ef2086f
dainji04 <ndai31072004@gmail.com>
2025-07-31 17:02:50 +0700

    frontend: init some component primary-btn and auth-intro

---
+++ frontend/src/app/components/primary-button/primary-button.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './primary-button.html',
+  styleUrl: './primary-button.scss',
+})
+export class PrimaryButton {
+  @Input() text: string = 'Click Me';
+}

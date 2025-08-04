2a750b64fa2104a8dc7834f6717fd15d499cf509
dainji04 <ndai31072004@gmail.com>
2025-08-04 09:23:53 +0700

    frontend: add loading when login and sign up

--- frontend/src/app/auth/login/login.ts
+++ frontend/src/app/auth/login/login.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import {
-    FormBuilder,
-    FormGroup,
-    ReactiveFormsModule,
-    Validators,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -12,65 +12,69 @@ import { PrimaryButton } from '../../components/primary-button/primary-button';
 import { AuthIntroComponent } from '../../components/auth-intro/auth-intro';
 
 @Component({
-    selector: 'app-login',
-    imports: [
-        PrimaryButton,
-        AuthIntroComponent,
-        CommonModule,
-        ReactiveFormsModule,
-        RouterModule,
-    ],
-    templateUrl: './login.html',
-    styleUrls: ['../auth.scss'],
+  selector: 'app-login',
+  imports: [
+    PrimaryButton,
+    AuthIntroComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+  templateUrl: './login.html',
+  styleUrls: ['../auth.scss'],
 })
 export class Login {
-    title: string = 'Login';
-    description: string =
-        'Log in to your account to start chatting with your friends.';
-    errorMessage: string = '';
-    formData!: FormGroup;
+  title: string = 'Login';
+  description: string =
+    'Log in to your account to start chatting with your friends.';
+  errorMessage: string = '';
+  formData!: FormGroup;
+  loading: boolean = false;
 
-    constructor(
-        private fb: FormBuilder,
-        // public themeService: ThemeService,
-        private authService: Auth,
-        private router: Router
-    ) {}
+  constructor(
+    private fb: FormBuilder,
+    // public themeService: ThemeService,
+    private authService: Auth,
+    private router: Router
+  ) {}
 
-    ngOnInit(): void {
-        this.formData = this.fb.group({
-            username: [
-                '',
-                [
-                    Validators.required,
-                    Validators.minLength(5),
-                    Validators.maxLength(10),
-                ],
-            ],
-            password: ['', [Validators.required, Validators.minLength(6)]],
-        });
+  ngOnInit(): void {
+    this.formData = this.fb.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(10),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    this.loading = true;
+    this.errorMessage = '';
+    if (this.formData.valid) {
+      const { username, password } = this.formData.value;
+      this.authService.login(username, password).subscribe({
+        next: (response: any) => {
+          console.log('Login successful', response);
+          this.loading = false;
+          this.router.navigate(['/']);
+        },
+        error: (error: any) => {
+          console.error('Signup failed', error.error.message);
+          this.errorMessage =
+            error.error.message || 'Signup failed. Please try again.';
+          this.loading = false;
+        },
+      });
+    } else {
+      console.error('Form is invalid');
+      // Mark all fields as touched to show validation messages
+      this.formData.markAllAsTouched();
+      this.loading = false;
     }
-
-    onSubmit(): void {
-        if (this.formData.valid) {
-            console.log('Form Data:', this.formData.value);
-            const { username, password } = this.formData.value;
-            this.authService.login(username, password).subscribe({
-                next: (response: any) => {
-                    console.log('Login successful', response);
-                    this.router.navigate(['/']);
-                },
-                error: (error: any) => {
-                    console.error('Signup failed', error.error.message);
-                    this.errorMessage =
-                        error.error.message ||
-                        'Signup failed. Please try again.';
-                },
-            });
-        } else {
-            console.error('Form is invalid');
-            // Mark all fields as touched to show validation messages
-            this.formData.markAllAsTouched();
-        }
-    }
+  }
 }
